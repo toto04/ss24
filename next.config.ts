7c89da7284e51fffb40504b6ebbb2960bc8de511
Tommaso Morganti <tommaso.morganti01@gmail.com>
2024-12-27 21:33:59 +0100

    workflow never gonna work the first time

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
